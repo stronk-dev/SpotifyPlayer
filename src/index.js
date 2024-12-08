20f6950400238a369ff20bea88cae6b6fb10fd85
Marco van Dijk <marco@stronk.rocks>
2024-12-08 17:07:06 +0100

    Center component nicely when standalone

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./shared.css";
+import "./standalone.css";
 
 const renderApp = () => {
   const container = document.getElementById("root");
